3990a050d1a364e82098c1a0972de70b1bda2b88
Wilkson Flor Soares <90102289+Wilksonflor@users.noreply.github.com>
2022-09-05 10:16:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-function openNav(){
-    alert('oi')
+var ul = document.querySelector('nav ul')
+var menuBtn = document.querySelector('.btnMobile i')
+
+function menuShow(){
+    if(ul.classList.contains('open')){
+        ul.classList.remove('open')
+    }else{
+        ul.classList.add('open')
+    }
 }
